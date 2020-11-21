281393694e641d974671d9f46235304afc6cc1fd
yharsh-0207 <yharsh-0207@users.noreply.github.com>
2020-11-21 21:30:14 +0530

    Create Pass_Fail.java

---
+++ Pass_Fail.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner;
+
+public class Pass_Fail {
+    
+    public static void main(String args[]){
+       int n;
+       Scanner reader = new Scanner(System.in);
+        System.out.println("Enter marks: ");
+         n = reader.nextInt();
+
+        if (n>40){
+
+           System.out.println("Pass");
+       }
+        else
+       System.out.println("Fail"); 
+    }
+}
