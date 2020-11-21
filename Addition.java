c016a9ee29ab74bad96e1544ccfae236d335a130
yharsh-0207 <yharsh-0207@users.noreply.github.com>
2020-11-21 20:23:53 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,23 @@
+import java.io.DataInputStream;
+
+public class Addition {
+    public static void main(final String args[]) {
+       
+        final DataInputStream in1 = new DataInputStream(System.in);
+
+          int a,b,c;
+
+         try {
+            System.out.println("Please enter the first number:");
+            a = Integer.parseInt(in1.readLine());
+
+            System.out.println("please enter second :");
+            b = Integer.parseInt(in1.readLine());
+
+            c = a + b;
+            System.out.println("result is: " + c);
+         } catch (final Exception e) {
+          System.out.println("Error: "+e);
+       }
+    }
+}
